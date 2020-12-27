ffb1e50a8b758151488cf524c60d6cbf17db0bf1
u.dukenbay <urziya.dukenbay@gmail.com>
2020-12-27 19:46:20 +0600

    add new Comments

--- src/components/post-list/post-list.js
+++ src/components/post-list/post-list.js
@@ -4,7 +4,7 @@ import PostListItem from '../post-list-item/post-list-item';
 
 import './post-list.css';
 
-const PostList = ({posts, onDelete}) => {
+const PostList = ({posts, onDelete, onToggleImportant, onToggleLiked}) => {
 
     const elements = posts.map((item) => {
         const {id, ...itemProps} = item;
@@ -13,6 +13,8 @@ const PostList = ({posts, onDelete}) => {
                 <PostListItem 
                     {...itemProps}
                     onDelete={() => onDelete(id)}
+                    onToggleImportant={() => onToggleImportant(id)}
+                    onToggleLiked={() => onToggleLiked(id)}
                 />
             </li>
         )
